326770c73c58c342f6c979be5224601d4c898e2f
webAKAsh <codingmetaltank@gmail.com>
2024-12-03 18:31:20 +0530

    type

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -9,9 +9,15 @@ const getCachedPost = cache((slug) => {
   });
 });
 
-export default async function PostPage({ params }) {
+export default async function PostPage({
+  params,
+}: {
+  params: {
+    slug: string;
+  };
+}) {
   const post = await getCachedPost(params.slug);
-  
+
   return (
     <main className="flex flex-col items-center gap-y-5 pt-24 text-center">
       <h1 className="text-3xl font-semibold">{post?.title}</h1>
