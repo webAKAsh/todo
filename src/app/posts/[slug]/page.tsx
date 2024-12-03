47b8cb10c9d72fb32efea77ab39dc6247ccd0519
webAKAsh <codingmetaltank@gmail.com>
2024-12-03 18:45:12 +0530

    new slug

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,13 +1,4 @@
 import prisma from "@/lib/db";
-import { unstable_cache as cache } from "next/cache";
-
-const getCachedPost = cache((slug) => {
-  return prisma.post.findUnique({
-    where: {
-      slug,
-    },
-  });
-});
 
 export default async function PostPage({
   params,
@@ -16,7 +7,11 @@ export default async function PostPage({
     slug: string;
   };
 }) {
-  const post = await getCachedPost(params.slug);
+  const post = await prisma.post.findUnique({
+    where: {
+      slug: params.slug,
+    },
+  });
 
   return (
     <main className="flex flex-col items-center gap-y-5 pt-24 text-center">
